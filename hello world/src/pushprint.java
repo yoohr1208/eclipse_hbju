a65fcfa78047dc2c151cbe634eabfc641ba0e958
yoohr1208 <73217226+yoohr1208@users.noreply.github.com>
2020-11-18 18:47:12 +0900

    Update pushprint.java

--- hello world/src/pushprint.java
+++ hello world/src/pushprint.java
@@ -1,7 +1,7 @@
 
 public class pushprint {
 	public static void main(String[] args) {
-		System.out.println("sophomore:2ÇÐ³â");
+		System.out.println("sophomore");
 		System.out.println("Jeonbuk Iksan");
 }
 }
